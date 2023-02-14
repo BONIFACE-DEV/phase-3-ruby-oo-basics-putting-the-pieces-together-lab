790b997fe6043a9af0f941f6727d2b728a3f5ed1
BONIFACE KORIR <boniface.korir@student.moringa.school@gmail.com>
2023-02-14 11:40:05 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,15 @@
 class Book
-    attr_accessor :author name ,:page count, :genre
-     attr_reader :title
- def  initialize title
-    @title=title
- end
-    def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-
+   attr_accessor :title, :author, :page_count, :genre
+   def initialize(title)
+       @title=title
+   end
+   def turn_page
+     puts  "Flipping the page...wow, you read fast!" 
+   end
 end
+
+first_book=Book.new("Mens Affair")
+first_book.author ="Softbuoy"
+first_book.genre="Awareness"
+first_book.page_count=900
+first_book.turn_page  
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,15 @@
-# Make your shoe class here!
 class Shoe
-    attr_accessor :color, :size, :material, :condition
-    attr_reader :brand
-  
-    def initialize(brand)
-      @brand = brand
-    end
-  
-    def cobble
-      @condition = "new"
-      puts "Your shoe is as good as new!"
-    end
+  attr_accessor :brand, :color, :size, :material, :condition
+  def initialize(brand)
+      @brand=brand
+      @condition="tattered"
   end
+  def cobble
+      puts "Your shoe is as good as new!"
+      @condition="new"
+  end
+end
+shoe = Shoe.new("timberland")
+shoe.color = "brown"
+shoe.size = 39
+shoe.condition = "used"
